6acac4f4f831cd79e540dc0ecc75e08db7634557
Elvira Miasnykova <miasnykovaelvira@gmail.com>
2022-10-13 14:03:38 +0300

    done with hook refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {useState } from "react";
 //import ReactDOM from "react-dom";
 import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
 import { Statistics } from "./Statistics/Statistics";
@@ -7,59 +7,85 @@ import { Section } from "./Section/Section";
 import { Box } from 'components/Box';
 
   
-  export class App extends Component {
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
     
-    state = {
-      good: 0,
-      neutral: 0,
-      bad: 0
-    };
+  const options = { good, neutral, bad };
+  
+  const keys = Object.keys(options);
+
+  //const options = [ 'good', 'neutral', 'bad' ];  // method with switch case
+
+  // const FeedbackItemCounter = feedbackItem => {
+  //   switch (feedbackItem) {
+  //     case 'good':
+  //       setGood(prevState => prevState + 1)
+  //       break;
+  //     case 'neutral':
+  //       setNeutral(prevState => prevState + 1)
+  //       break;
+  //     case 'bad':
+  //       setBad(prevState => prevState + 1)
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // };
     
-    FeedbackItemCounter = options => {
-      this.setState(prevState => {
-        return {
-          [options]: prevState[options] + 1,
+    
+  const FeedbackItemCounter = options => {
+    if (options === keys[0]) {
+      setGood(prevState => prevState + 1);
+    } else
+      if (options === keys[1]) {
+        setNeutral(prevState => prevState + 1);
+      } else
+        if (options === keys[2]) {
+          setBad(prevState => prevState + 1);
+        }
+      
 
         };
-      })
-    };
+     
 
-    FeedbackTotalValueCounter = () => {
+    const FeedbackTotalValueCounter = () => {
       return(
-        this.state.good + this.state.neutral + this.state.bad
+        good + neutral + bad
       )
     };
 
-    FeedbackPercentageCounter = () => {
+    const FeedbackPercentageCounter = () => {
       return(
-        (this.state.good / this.FeedbackTotalValueCounter()) * 100
+        (good / FeedbackTotalValueCounter()) * 100
       )
     };
 
-    render() {
-      const total = this.FeedbackTotalValueCounter();
-      const positive = Math.floor(this.FeedbackPercentageCounter());
+    
+      const total = FeedbackTotalValueCounter();
+      const positive = Math.floor(FeedbackPercentageCounter());
       
       return (
         <Box pt={3} pb={3} pl={4} >
           <Section title={"Please leave feedback"}>
             <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.FeedbackItemCounter} />
+            options={keys}
+            onLeaveFeedback={FeedbackItemCounter} />
           </Section>
           <Section title={"Statistics"}>
             {total > 0 ?
           <Statistics 
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
+            good={good}
+            neutral={neutral}
+            bad={bad}
             total={total}
               positivePercentage={positive} />
             : <Notification message={"There is no feedback"} />}
           </Section>
         </Box>
       );
-    };
+    
   }
 
   // `<h2>Please leave feedback</h2>
